f3c23054673dcbc00d9d34c60a7d123012f0d24c
Yifu Zhou <efuu1937@gmail.com>
2017-02-21 15:15:54 -0600

    Add files via upload

--- Reversi/src/drawUI.java
+++ Reversi/src/drawUI.java
@@ -4,28 +4,44 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class drawUI extends JFrame {
+	public static int[][] save_board;
     
-	public static void print(Reversi game, JPanel panel) {
+	public static void print(Reversi game, JPanel panel, JLabel pieces) {
 		for (int y = 0; y < 8; y++) {
 	    	   for (int x = 0; x < 8; x++) {
-	    		   JLabel pieces = new JLabel(); 
-	    		   if (game.board[y][x] == 0) {
-	    			   pieces.setIcon(new ImageIcon("graph/empty.jpg"));
-	        	       panel.add(pieces, 0);
+	    		   if (game.board[y][x] != save_board[y][x]) {
+	    			   if (game.board[y][x] == -1) {
+	    				   ImageIcon image2 = new ImageIcon("graph/black.jpg");
+  	       		    	   pieces.setIcon(image2);
+		    		   }
+	    			   if (game.board[y][x] == 1) {
+	    				   ImageIcon image2 = new ImageIcon("graph/white.jpg");
+  	       		    	   pieces.setIcon(image2);
+	    			   }   
 	    		   }
-	    		   if (game.board[y][x] == -1) {
-	    			   pieces.setIcon(new ImageIcon("graph/black.jpg"));
-	        	       panel.add(pieces, 0);
-	    		   }
-	    		   if (game.board[y][x] == 1) {
-	    			   pieces.setIcon(new ImageIcon("graph/white.jpg"));
-	        	       panel.add(pieces, 0);
-	    		   }
-	    	       Dimension size_p = pieces.getPreferredSize();
-	    	       pieces.setBounds(26 + x * 37, 26 + y * 37, size_p.width, size_p.height);
 	    	   }
-		}	
+		}
 	}
+	    		   
+	    		   
+//	    		   JLabel pieces = new JLabel(); 
+//	    		   if (game.board[y][x] == 0) {
+//	    			   pieces.setIcon(new ImageIcon("graph/empty.jpg"));
+//	        	       panel.add(pieces, 0);
+//	    		   }
+//	    		   if (game.board[y][x] == -1) {
+//	    			   pieces.setIcon(new ImageIcon("graph/black.jpg"));
+//	        	       panel.add(pieces, 0);
+//	    		   }
+//	    		   if (game.board[y][x] == 1) {
+//	    			   pieces.setIcon(new ImageIcon("graph/white.jpg"));
+//	        	       panel.add(pieces, 0);
+//	    		   }
+//	    	       Dimension size_p = pieces.getPreferredSize();
+//	    	       pieces.setBounds(26 + x * 37, 26 + y * 37, size_p.width, size_p.height);
+//	    	   }
+//		}	
+//	}
 
    public static void main(String[] args) {
        JFrame frame=new JFrame("Reversi");  //title
@@ -43,6 +59,7 @@ public class drawUI extends JFrame {
        label.setBounds(0, 0, size.width, size.height);
   
        Reversi game = new Reversi();
+       save_board = Algorithm.copy2Darray(game.board);
        for (int y = 0; y < 8; y++) {
     	   for (int x = 0; x < 8; x++) {
     		   JLabel pieces = new JLabel(); 
@@ -69,9 +86,12 @@ public class drawUI extends JFrame {
 //    		    	System.out.println("x is : " + pieces.getText());
 //    		    	ImageIcon image2 = new ImageIcon("graph/white.jpg");
 //    		    	pieces.setIcon(image2);
+    		    	Rule.printBoard(game.board);
     		    	game.printPlayerMove(pieces.getText());
-    		    	print(game, panel);
-    		    	game.AIreaction();
+    		    	Rule.printBoard(save_board);
+    		    	Rule.printBoard(game.board);
+    		    	print(game, panel, pieces);
+    		    	//game.AIreaction();
     		    	//print(game, panel);
     		    
     		    	
